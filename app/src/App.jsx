c581d4646dfbf5b66043b11f8bf78ed94f71afda
Ram-Ram007 <ramajayam2411@gmail.com>
2023-10-06 02:24:58 +0530

    final commit

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,71 +1,126 @@
-import React, { useState } from "react";
-import "./App.css";
-import Input from "./Components/Input";
-import Button from "./Components/Button";
-import DisplayFormData from "./Components/DisplayFormData"; // Import the DisplayFormData component
+import React, { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import Input from './Components/input'; 
+import Button from './Components/button'; 
+import * as Yup from 'yup';
+import './App.css';
+
+
 
 function App() {
-  const [formData, setFormData] = useState({
-    title: "",
-    description: "",
-    rating: "",
-    imageUrl: "",
-  });
-
-  const [formValues, setFormValues] = useState(null);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+  const initialValues = {
+    title: '',
+    description: '',
+    ratings: '',
+    url: '',
   };
 
-  const handleSubmit = () => {
-    setFormValues(formData);
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required('Title required'),
+  description: Yup.string().required('Description required').min(5,"Description should be at least 5 characters"),
+  ratings: Yup.number().required('Ratings is required').max(10,"Rating cannot be greater than 10"),
+  url: Yup.string().url('Enter a valid URL').required('URL is required'),
+});
+
+
+
+  const handleSubmit = (values, { resetForm }) => {
+    
+    console.log(values);
+
+    
+    const url = document.createElement('img');
+    url.src = values.url;
+    url.alt = 'Image';
+
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const title = document.createElement('h3');
+    title.textContent = values.title;
+
+    const ratings = document.createElement('p');
+    ratings.textContent = `Ratings: ${values.ratings}`;
+
+    const description = document.createElement('p');
+    description.textContent = values.description;
+
+    
+
+   
+
+    card.append(title, description, ratings, url);
+
+
+    
+    document.querySelector(".card").appendChild(card);
+
+
+    resetForm();
   };
 
   return (
-    <div className="App">
-      <div className="form-container">
-        <Input
-          name="title"
-          type="text"
-          placeholder="Enter the movie title"
-          onChange={handleInputChange}
-        />
-        <Input
-          name="description"
-          type="text"
-          placeholder="Enter the movie description"
-          onChange={handleInputChange}
-        />
-        <Input
-          name="rating"
-          type="number"
-          placeholder="Enter the movie rating"
-          onChange={handleInputChange}
-        />
-        <Input
-          name="imageUrl"
-          type="url"
-          placeholder="Enter the image URL"
-          onChange={handleInputChange}
-        />
-        <Button
-          onClick={handleSubmit}
-          name="Submit"
-        />
-      </div>
-
-      {formValues && (
-        <>
-          <DisplayFormData formData={formValues} />
-          {formValues.imageUrl && (
-            <div className="image-container">
-              <img src={formValues.imageUrl} alt="Movie Poster" />
+    <div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ touched, errors }) => (
+          <Form>
+            <div>
+              <label htmlFor="title">Title</label>
+              <Field
+                type="text"
+                id="title"
+                name="title"
+                placeholder="Title"
+              />
+              {touched.title && errors.title && (
+                <div className="error">{errors.title}</div>
+              )}
             </div>
-          )}
-        </>
-      )}
+            <div>
+              <label htmlFor="description">Description</label>
+              <Field
+                type="text"
+                id="description"
+                name="description"
+                placeholder="Description"
+              />
+              {touched.description && errors.description && (
+                <div className="error">{errors.description}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="ratings">Ratings</label>
+              <Field
+                type="number"
+                id="ratings"
+                name="ratings"
+                placeholder="Ratings"
+              />
+              {touched.ratings && errors.ratings && (
+                <div className="error">{errors.ratings}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="url">URL</label>
+              <Field
+                type="url"
+                id="url"
+                name="url"
+                placeholder="URL"
+              />
+              {touched.url && errors.url && (
+                <div className="error">{errors.url}</div>
+              )}
+            </div>
+            <Button type="submit" name="Submit" />
+          </Form>
+        )}
+      </Formik>
+      <div className="card"></div>
     </div>
   );
 }
